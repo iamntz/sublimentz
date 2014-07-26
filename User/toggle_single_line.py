675545d6c7e746bc83d1f68d524b6a9ffc573caa
Ionut Staicu <github@iamntz.com>
2014-07-26 08:47:34 +0300

    small tweaks

--- User/toggle_single_line.py
+++ User/toggle_single_line.py
@@ -7,8 +7,18 @@ class ToggleSingleLineCommand(sublime_plugin.TextCommand):
       # check if the css statement needs to be expanded or collapsed
       if re.match('^.*\{.*}\s*$', text):
         # expand the css statement
-        m = re.search('^(?P<key>.*)\{(?P<params>.*)\;\s*}$', text)
-        multiline = '%s{\n\t%s;\n}' % (m.group('key'), m.group('params').strip().replace('; ', ';\n\t'))
+        m      = re.search('^(?P<key>.*)\{(?P<params>.*)\;\s*}$', text)
+        indent = '\n\t';
+        key    = m.group('key');
+        params = m.group('params').strip().replace(';', ';' + indent)
+
+        multiline = '%s{%s%s;}' % (
+          key,
+          indent,
+          params
+        )
+        # insert should be used instead
+        # self.view.insert(edit, region, "Hello, World!")
         self.view.replace(edit, region, multiline)
       else:
         # collapse the css statement
